a2aa0ba7f831bc0c3c87d71f26666398a60a07d7
Nihat571 <nihatum-ab206@code.edu.az>
2025-03-18 13:02:29 -1200

    added hometask3

---
+++ Human.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask3;
+
+class Human
+{
+    public string name;
+    public string surName;
+    public byte age;
+
+    public Human()
+    {
+        
+    }
+    public Human(string name,string surName,byte age)
+    {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+    }
+
+    public void ShowFullData()
+    {
+        Console.WriteLine($"name:{name}\nsurname:{surName}\nage:{age}");
+    }
+
+}
+class User(string name):Human
+{
+    string userName = name;
+    string password;
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HomeTask3;
+
+Human human = new Human("Nihat","Memmedov",19);
+//human.ShowFullData();
+User user = new User("Nihat");
